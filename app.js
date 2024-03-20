946427bb7707ece581975d0ded45afa344e66323
Manil munjal <111218259+manilmunjal@users.noreply.github.com>
2024-03-20 17:08:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello, World!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Use environment variable for port
+
+app.get('/', (req, res) => {
+  res.send('Hello, GitHub Actions!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
 });
